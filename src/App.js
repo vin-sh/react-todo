fd8476c1e5f17ab2b4019eed7e2ae48e908b38ff
vin-sh <vinayak315singh@gmail.com>
2019-08-25 14:54:08 +0530

    Changing state data

--- src/App.js
+++ src/App.js
@@ -8,13 +8,25 @@ export default class App extends React.Component {
                 this.state = {
                     userName: "Vinayak"
         }
-    }    
+    }
+    
+    changeStateData = () => {
+                this.setState({
+                    userName: this.state.userName === "Vinayak" ? "Ronnie" : "Vinayak"
+                })
+    }
+    
     render() {
         return (
             <div>
                 <h4 className="bg-primary text-white text-center p-2">
                     { this.state.userName }'s To Do List
                 </h4>
+
+                <button className="btn btn-primary m-2"
+                        onClick={ this.changeStateData }>
+                    Change
+                </button>
             </div>
         )
     };
